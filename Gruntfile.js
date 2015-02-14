b8eb0d25b72ace6a3434024569e3559c4b654773
pier0n <andrzej.dubiel@gmail.com>
2015-02-13 19:56:13 -0600

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+module.exports = function(grunt) {
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.config.init({
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: './build/'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      scripts: {
+        files: ['source/scripts/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false,
+        }
+      },
+      images: {
+        files: ['images/**/*.{png,jpg,gif,svg}', 'images/*.{png,jpg,gif,svg}'],
+        tasks: ['imagemin', 'svgmin'],
+        options: {
+          spawn: false,
+        }
+      },
+      html:{
+        files: ['./**/*.html'],
+        tasks: ['htmlmin'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    concat: {
+      build: {
+        src: [
+        'source/js/libs/*.js',
+        'source/js/global.js'
+        ],
+        dest: 'build/scripts/production.js'
+      }
+    },
+    uglify: {
+      build: {
+        src: 'build/scripts/production.js',
+        dest: 'build/scripts/production.min.js'
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'source/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'build/images/'
+        }]
+      }
+    },
+    svgmin: {
+      images: {
+        expand: true,
+        cwd: 'source/images/',
+        src: ['**/*.svg'],
+        dest: 'build/images/'
+      }
+    },
+    less: {
+      build: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          'build/styles/app.min.css': 'source/less/app.less'
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version']
+      },
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'build/styles/*.css',
+        dest: 'build/styles/'
+      }
+    },
+    htmlmin: {                                     
+      build: {                                      
+        options: {                                 
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   
+          'build/index.html': 'source/views/index.html',    
+        }
+      }
+    }
+});
+
+grunt.registerTask('default', ['concat', 'uglify', 'less', 'imagemin', 'svgmin', 'autoprefixer', 'htmlmin']);
+grunt.registerTask('dev', ['connect', 'watch']);  
+
+
+};
