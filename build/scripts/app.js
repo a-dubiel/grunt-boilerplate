e0733e4a4157f4539eb8ebb3819e7841e31ccb5a
pier0n <andrzej.dubiel@gmail.com>
2015-02-14 21:42:16 -0600

    Bumped the version

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,6 +1,6 @@
 /**
  * Just another Grunt boilerplate v0.0.1
- * Saturday, February 14th, 2015, 8:29:47 PM
+ * Saturday, February 14th, 2015, 8:41:12 PM
  * 
  * 2015 Andrzej Dubiel | http://adubiel.me/
  */
--- grunt/copy.js
+++ grunt/copy.js
@@ -2,7 +2,7 @@ exports.task = {
   bowerjs: {       
     expand: true,
     flatten: true,
-    src: '<%= path.source %>/<%= path.components %>/<%= path.scripts %>/**/*.js',
+    src: '<%= path.source %>/<%= path.components %>/**/*.js',
     dest: '<%= path.build %>/<%= path.scripts %>/<%= path.components %>/', 
     filter: 'isFile'   
   }
